9c9ec6394c3d72a49db53cb8b077bf18d9eba49b
njacke <75493946+njacke@users.noreply.github.com>
2020-12-22 11:50:20 +0100

    v0.0.1
    - Started the project
    - Created splash screen
    - Created start screen
    - Created play space

---
+++ glitch-garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    [SerializeField] float loadDelay = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(LoadStartWithDelay());
+        }
+    }
+
+    private IEnumerator LoadStartWithDelay()
+    {
+        yield return new WaitForSeconds(loadDelay);
+        SceneManager.LoadScene(1);
+    }
+}
