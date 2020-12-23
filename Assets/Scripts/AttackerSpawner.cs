1af41f2d91f65311551fe0aa3617033ec390d2be
njacke <75493946+njacke@users.noreply.github.com>
2020-12-23 13:54:12 +0100

    v0.0.2
    - Added lizard (attacker) prefab and animation
    - Added enemy spawner
    - Added cactus (defender) prefab and animation
    - Added trophy (resource) prefab and animation
    - Added zucchini (projectile)

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+
+
+    [SerializeField] bool spawn = true;
+
+
+    [SerializeField] float minSpawnTime = 1f;
+    [SerializeField] float maxSpawnTime = 5f;
+    [SerializeField] GameObject enemy;
+
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
